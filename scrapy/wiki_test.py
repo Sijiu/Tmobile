9b3cd3e8271bdaccba5169df190cee2e5e15d107
Plain <mxh403@163.com>
2016-05-19 09:04:53 +0800

    wiki_test

--- scrapy/wiki_test.py
+++ scrapy/wiki_test.py
@@ -3,20 +3,43 @@
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 from wikisql import Wiki_first
+from lib.model.session import sessionCM
 import re
 
 pages = set()
 def getlinks(pageUrl):
     global pages
-    html = urlopen("https://zh.wikipedia.org" + pageUrl)
+    real_url = "https://zh.wikipedia.org" + pageUrl
+    # real_url = "https://en.wikipedia.org" + pageUrl
+    html = urlopen(real_url)
     bsObj = BeautifulSoup(html)
+    flag = ""
     try:
-        print "h1---", bsObj.h1.get_text()
-        print "content--", bsObj.find(id="mw-content-text").findAll("p")[0]
-        print "edit--", bsObj.find(id="ca-edit").find("span").find("a").attrs["href"]
-    except AttributeError:
-        print "页面缺少一些属性，不过不用太担心！"
+        h1 = bsObj.h1.get_text()
+        content = bsObj.find(id="mw-content-text").findAll("p")[0]
+        edit = bsObj.find(id="ca-edit").find("span").find("a").attrs["href"]
 
+        print "h1---", h1
+        print "content--", content
+        print "edit--", edit
+    except AttributeError:
+        flag = "wrong"
+        print "页面缺少一些属性，不过不用太担心！"
+    except IndexError:
+        flag = "wrong"
+        print "list index out of range, content"
+    try:
+        with sessionCM() as session:
+            print "---", flag, len(flag)
+            if not len(flag):
+                Wiki_first.create(session, h1=h1, content=content, edit=edit, url=real_url)
+                print "pages insert"
+            else:
+                Wiki_first.create(session, h1="", content="The page is missing something, Don't worry!"
+                                  , edit="", url=real_url)
+                print "notes insert"
+    except Exception:
+        print "Insert Failed!"
     bslink = bsObj.findAll("a", href = re.compile("^(/wiki/)"))
     for link in bslink:
         if "href" in link.attrs:
@@ -27,4 +50,7 @@ def getlinks(pageUrl):
                 pages.add(newPage)
                 getlinks(newPage)
 
-getlinks('/wiki/網路蜘蛛')
+
+# getlinks('/wiki/User:Yhz1221')
+# getlinks('/wiki/Scrapy')
+getlinks('/wiki/Python')
--- scrapy/wikisql.py
+++ scrapy/wikisql.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 class Wiki_first(Base):
     __tablename__ = "wiki_first"
     id = SA.Column(SA.INTEGER, primary_key=True, autoincrement=True)
-    h1 = SA.Column(SA.INTEGER)
+    h1 = SA.Column(SA.String(128))
     content = SA.Column(SA.TEXT)  # max = 21845
     edit = SA.Column(SA.String(1024))
     url = SA.Column(SA.String(1024))
