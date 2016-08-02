3c63534426dff692c5f5f91e347cb37dd0af15ad
Plain <mxh403@163.com>
2016-08-03 01:58:17 +0800

    teseract

---
+++ scrapy/lago_captcha_.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+from PIL import Image
+import pytesseract
+
+try:
+    print "successfully"
+except ImportError:
+    print '模块导入错误,请使用pip安装,pytesseract依赖以下库：'
+    print 'http://www.lfd.uci.edu/~gohlke/pythonlibs/#pil'
+    print 'http://code.google.com/p/tesseract-ocr/'
+    raise SystemExit
+
+image = Image.open('F:/github/Tmobile/scrapy/captcha_0.26806422253139317.jpg')
+vcode = pytesseract.image_to_string(image)
+print vcode
